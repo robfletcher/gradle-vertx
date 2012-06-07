b7fcaf6572eafd3bc2ff39cf88be750b3a768c46
Rob Fletcher <robert.w.fletcher@gmail.com>
2012-06-08 00:07:27 +0200

    use TaskExecutionListener to manage vertx lifecycle
    Would be nice to package this up properly so you could just declare the tasks that require vertx to be running

--- src/integration-test/groovy/io/vertx/gradle/VertxDeploySpec.groovy
+++ src/integration-test/groovy/io/vertx/gradle/VertxDeploySpec.groovy
@@ -6,7 +6,7 @@ class VertxDeploySpec extends Specification {
 
 	void 'can interact with a deployed verticle'() {
 		expect:
-		new URL('http://localhost:8080').text == '!<!doctype html><html><head><title>Vert.x Test</title></head><body><p><em>Vert.x</em> is alive!</p></body></html>'
+		new URL('http://localhost:8080').text == '<!doctype html><html><head><title>Vert.x Test</title></head><body><p><em>Vert.x</em> is alive!</p></body></html>'
 	}
 
 }
